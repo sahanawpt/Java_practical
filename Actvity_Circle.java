cd8c7d2b2aa3de1a2b30d1a7bf6f3e6dd5eebbd9
sahanawpt <161597738+sahanawpt@users.noreply.github.com>
2024-04-20 14:29:08 +0530

    Activity Week_3 Define a class named Circle with attributes radius. Implement methods to calculate the area and circumference of the circle. Instantiate an object of the Circle class by using default constructor and demonstrate how to access and modify its attributes.

---
+++ Actvity_Circle.java
@@ -0,0 +1,27 @@
+package Activity;
+
+public class Actvity_Circle {	    
+	    private double radius;
+	    public Actvity_Circle() {
+	        this.radius = 1.0;
+	    }
+	    public Actvity_Circle(double radius) {
+	        this.radius = radius;
+	    }
+	    public double calculateArea() {
+	        return Math.PI * radius * radius;
+	    }
+	    public double calculateCircumference() {
+	        return 2 * Math.PI * radius;
+	    }
+
+	    public static void main(String[] args) {
+	        Actvity_Circle circle = new Actvity_Circle();
+	        System.out.println("Initial radius: " + circle.radius);
+	        circle.radius = 15.0; // 
+	        System.out.println("Modified radius: " + circle.radius);
+	        System.out.println("Area: " + circle.calculateArea());
+	        System.out.println("Circumference: " + circle.calculateCircumference());
+	    }
+}
+
